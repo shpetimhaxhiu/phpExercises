0bfed5e3a90834904c3d16f23231c1f2d4905086
Shpetim Haxhiu <shpetim.h@gmail.com>
2018-04-05 16:45:57 +0100

    Triple Equals Exercises

---
+++ triple_equals.php
@@ -0,0 +1,17 @@
+<?php
+
+$num1 = 7;
+$num2 = '7';
+
+if($num1 == $num2) {
+  echo "num1 == num2<br>";
+} else {
+  echo "num1 != num2<br>";
+}
+if($num1 === $num2) {
+  echo "num1 === num2<br>";
+} else {
+  echo "num1 !== num2<br>";
+}
+
+?>
