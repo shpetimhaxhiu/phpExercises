7e124c1bfe26964add34302697daf186bf1e3e5f
Shpetim Haxhiu <shpetim.h@gmail.com>
2018-04-05 16:37:01 +0100

    If Statement Exercises

---
+++ if_statement.php
@@ -0,0 +1,20 @@
+<?php
+
+$password = 'password';
+
+if ($password == "password") {
+  echo "Passwordi Okay<br>";
+} else {
+  echo "Passwordi Gabim<br>";
+}
+
+
+$mosha = 27;
+
+if($mosha > 21) {
+  echo "Its okay<br>";
+} else {
+  echo "You have to be older!<br>";
+}
+
+?>
