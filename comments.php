454c07f4c1cb7b2a05c5d319877db65b209ce3cd
Shpetim Haxhiu <shpetim.h@gmail.com>
2018-04-05 16:22:02 +0100

    Comments Exercises

---
+++ comments.php
@@ -0,0 +1,18 @@
+<?php
+// This is a comment
+//
+// This is another comment
+
+/* This is also a comment */
+
+/*
+this is a comment also
+
+this is multiline comment
+*/
+
+
+
+// Comment
+echo 'Pito' // this is also a inline comment
+?>
